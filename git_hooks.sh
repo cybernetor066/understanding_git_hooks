acbbe85e828a7a81be5cc7684ad5d1f30fb6529f
Anetor Eguakhide Kunle <cybernetortechnologies@gmail.com>
2023-02-05 14:14:16 +0000

    Testing git hooks commit msg(Adding pre-commit check)

--- git_hooks.sh
+++ git_hooks.sh
@@ -17,15 +17,20 @@
 
 
 # This stops commits with empty messages
-COMMIT_MESSAGE=`cat ../COMMIT_EDITMSG`;
+COMMIT_MESSAGE=`cat ./.git/COMMIT_EDITMSG`;
+#COMMIT_MESSAGE="Text present"
+#COMMIT_MESSAGE=""
 
-if [ $COMMIT_MESSAGE != "" ]
+echo "DEBUG 1"
+
+if [ "$COMMIT_MESSAGE" = "" ]
 then
         echo "Must Have A Commit Msg!"
         exit 1
 fi
 
 
+# Testing starts now
 
 
 
